8add23c14e558cc3fb3ed1fe5d968f651d1f9f4f
Alex Turk <alexturk4@gmail.com>
2019-12-02 17:08:00 -0600

    adjacency list construction working

--- src/com/company/Graph.java
+++ src/com/company/Graph.java
@@ -13,17 +13,45 @@ public class Graph {
 
     void addEdge(String origin, String destination, int time, int cost){
         Node original = new Node(origin, destination, time, cost);
+
         // since the graph is undirected, we will add a node containing the reverse direction also
         Node reverse = new Node(destination, origin, time, cost);
 
-        LinkedList<Node> sublist1 = new LinkedList<>();
-        LinkedList<Node> sublist2 = new LinkedList<>();
+        // flags to determine if the node has already been added into the list
+        Boolean addOrig = false, addRev = false;
 
-        sublist1.add(original);
-        sublist2.add(reverse);
+        // We must first iterate through outer list
+        for (int i = 0; i < list.size(); i++) {
+            LinkedList<Node> sublist = list.get(i);
 
-        list.add(sublist1);
-        list.add(sublist2);
+            // if the first element in the sublist has the same origin as our new node, then add it to this list
+            if (list.get(i).get(0).getOrigin().equals(origin)){
+                list.get(i).add(original);
+                addOrig = true;
+            }
+
+            // do the same for the destination since this is an undirected graph
+            if (list.get(i).get(0).getOrigin().equals(destination)){
+                list.get(i).add(reverse);
+                addRev = true;
+            }
+
+        }
+
+        // if we iterated through and the origin city is not in the list, then add it
+        if (!addOrig){
+            LinkedList<Node> sublist = new LinkedList<>();
+            sublist.add(original);
+            list.add(sublist);
+        }
+
+        // if we iterated through and the destination city is not in the list, then add it
+        if (!addRev){
+            LinkedList<Node> sublist = new LinkedList<>();
+            sublist.add(reverse);
+            list.add(sublist);
+        }
+
     }
 
 
